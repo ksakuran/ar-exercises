57cf05d04ba4292bc1e2f099567bf0bd02644917
Kat Nishimura <katherine.nishimura@live.ca>
2023-03-07 03:02:14 +0000

    create associations between models, add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Janice", last_name: "Hosenstein", hourly_rate: 60)
+@store1.employees.create(first_name: "Ross", last_name: "Geller", hourly_rate: 70)
+@store2.employees.create(first_name: "Monica", last_name: "Geller", hourly_rate: 65)
+@store2.employees.create(first_name: "Joey", last_name: "Tribbiani", hourly_rate: 55)
+@store1.employees.create(first_name: "Chandler", last_name: "Bing", hourly_rate: 55)
+@store2.employees.create(first_name: "Rachel", last_name: "Green", hourly_rate: 70)
+@store2.employees.create(first_name: "Phoebe", last_name: "Buffay", hourly_rate: 65)
+@store1.employees.create(first_name: "Richard", last_name: "Burke", hourly_rate: 70)
+@store2.employees.create(first_name: "Mike", last_name: "Hannigan", hourly_rate: 60)
+@store1.employees.create(first_name: "Gunther", last_name: "Central-Perk", hourly_rate: 70)
